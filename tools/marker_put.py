3e8def3a9fce095deda059ea276cccb76026d005
SUGIMOTO Norimitsu <dictoss@live.jp>
2016-05-04 10:24:18 +0900

    change http library to requests. - adjust same code python2 and python3.

--- tools/marker_put.py
+++ tools/marker_put.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 import sys
 import json
 import datetime
 import time
-import urllib.request
-import urllib.parse
-import urllib.error
+import requests
 
 MP_URL = 'http://192.168.22.102:8000/markerstorage/markerdata/0/'
 #MP_URL = 'http://192.168.22.30/osmproj1/markerstorage/markerdata/0/'
@@ -26,16 +24,16 @@ def put_markerobject():
 
     try:
         query_args = {'password': MP_POST_PASS}
-        encode_url_param = urllib.parse.urlencode(query_args)
+        header_args = {'Content-Type': 'application/json; charset=utf-8'}
 
-        req = urllib.request.Request('%s?%s' % (MP_URL, encode_url_param))
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
-        req.method = 'POST'
-        req.data = json.dumps(payload).encode()
+        print('try send PUT request.')
+        r = requests.post(MP_URL,
+                          params=query_args,
+                          headers=header_args,
+                          data=json.dumps(payload).encode())
 
-        r = urllib.request.urlopen(req)
-        print('send PUT request.')
-        print(('status code: %s' % (r.code)))
+        print(('status code: %s' % (r.status_code)))
+        print(r.text)
     except:
         print(('EXCEPT: fail PUT (%s)' % sys.exc_info()[1]))
 
