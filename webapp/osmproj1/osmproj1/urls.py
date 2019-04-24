d33550b69400a81aa44661462a451ee054772458
SUGIMOTO Norimitsu <dictoss@live.jp>
2019-04-24 21:37:06 +0900

    fix RemovedInDjango20Warning - Passing a 3-tuple to django.conf.urls.include() is deprecated. Pass a 2-tuple containing the list of patterns and app_name, and provide the namespace argument to include() instead.

--- webapp/osmproj1/osmproj1/urls.py
+++ webapp/osmproj1/osmproj1/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # url(r'^$', 'osmproj1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^markerstorage/', include('markerstorage.urls')),
 ]
